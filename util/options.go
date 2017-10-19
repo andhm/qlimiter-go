9d2329896c34c05a10a84c27daab1448e435375a
黄敏 <andhm@126.com>
2017-10-19 00:06:24 -0500

    增加配置选项

--- util/options.go
+++ util/options.go
@@ -17,6 +17,11 @@ type Options struct {
     SepNum          uint16
 
     ChannelQueueCap uint
+    ProcessTimeout  uint
+
+    DebugHttpServeIp    string
+    DebugHttpServePort  int
+
 }
 
 func NewOptions() *Options {
@@ -29,25 +34,35 @@ func (opts *Options) Parse() error {
     channelQueueCap := flag.Int("channelqueuecap", 10240, "queue capacity of channel")
 
     logDir          := flag.String("logdir", "", "directory for log file")
-    logLevel        := flag.String("loglevel", "info", "log level")
+    logLevel        := flag.String("loglevel", "info", "log level, e.g. debug info warn error fatal ")
+    processTimeout  := flag.Int("processtimeout", 1000, "millisecs of process single request")
+
+    debugHttpServeIp    := flag.String("debughttpserveip", "127.0.0.1", "http server ip when loglevel=debug")
+    debugHttpServePort  := flag.Int("debughttpserveport", 6060, "http server port when loglevel=debug")
 
     flag.Parse()
 
     // TODO valid opts
-    opts.Port       = *port
-    opts.SepNum     = uint16(*sepNum)
+    opts.Port               = *port
+    opts.SepNum             = uint16(*sepNum)
     opts.ChannelQueueCap    = uint(*channelQueueCap)
-    opts.LogDir     = *logDir
-    opts.LogLevel   = *logLevel
+    opts.LogDir             = *logDir
+    opts.LogLevel           = *logLevel
+    opts.ProcessTimeout     = uint(*processTimeout)
+    opts.DebugHttpServeIp   = *debugHttpServeIp
+    opts.DebugHttpServePort = *debugHttpServePort
 
     return nil
 }
 
 func (opts *Options) String() string {
-    return fmt.Sprintf("port[%d], loglevel[%s], logdir[%s], sepnum[%v], channelqueuecap[%v]", 
+    return fmt.Sprintf("port[%d], loglevel[%s], logdir[%s], sepnum[%v], channelqueuecap[%v], processtimeout[%v], debughttpserveip[%s], debughttpserveport[%d]", 
         opts.Port, 
         opts.LogLevel, 
         opts.LogDir, 
         opts.SepNum, 
-        opts.ChannelQueueCap)
+        opts.ChannelQueueCap,
+        opts.ProcessTimeout,
+        opts.DebugHttpServeIp,
+        opts.DebugHttpServePort)
 }
